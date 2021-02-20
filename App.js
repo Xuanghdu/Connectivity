d05f713bfe3197698bc0359e10d4fb668ce4bcd1
LittleDijkstraZ <54603864+LittleDijkstraZ@users.noreply.github.com>
2021-02-20 03:02:30 -0500

    mainfile change

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Me } from './src/components/Me';
+import { Login_screen } from './src/components/Login/Login_sreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Welcome /> */}
-      {/* <Login /> */}
-      <Me />
+      <Login_screen />
+      {/* <Me /> */}
       <StatusBar style="auto" />
     </View>
   );
