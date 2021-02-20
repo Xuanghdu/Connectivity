e2f7da0e7b02b095c4434b3b013e8e299885a168
LittleDijkstraZ <54603864+LittleDijkstraZ@users.noreply.github.com>
2021-02-20 02:10:02 -0500

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,45 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 export class Login extends React.Component {
+
     render() {
         return (
             <View style={styles.container}>
-                <Text style={{ marginBottom: 10 }}>Login</Text>
+                <View style={{ backgroundColor: 'orange' }}>
+                    <Image source={require('./pic.png')} />
+
+                </View>
                 <TextInput
                     placeholder='Username'
                     style={styles.input} />
                 <TextInput
                     placeholder='Password'
                     style={styles.input} />
-                <Greeting name="Xuanghdu" />
-                <StatusBar style="auto" />
-            </View>
+                <Button title='Login' style={styles.button} />
+            </View >
         );
     }
 }
 
-const Greeting = (props) => {
-    return (
-        <view style={styles.center}>
-            <Text>Welcome to Connectivity, {props.name}!</Text>
-        </view>
-    );
-}
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'dodgerblue',
+        backgroundColor: 'orange',
         alignItems: 'center',
         justifyContent: 'center',
     },
     input: {
         height: '5%',
         backgroundColor: 'rgba(255,255,255,0.7)',
-        marginBottom: 10
+        marginBottom: 10,
 
+    },
+    button: {
+        elevation: 8,
+        backgroundColor: "#009688",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 12
     }
 });
