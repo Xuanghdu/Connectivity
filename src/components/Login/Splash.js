34d13498bb36fa2c95d279a3f5b46c9b4aa00967
LittleDijkstraZ <54603864+LittleDijkstraZ@users.noreply.github.com>
2021-02-20 10:26:59 -0500

    cneter splash

--- src/components/Login/Splash.js
+++ src/components/Login/Splash.js
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
         margin: '10px',
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'bottom',
+        justifyContent: 'center',
     },
     avatar: {
         width: 100,
