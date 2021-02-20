787d65790f2d21e21200d726098b5f228c3b68c0
Xuanghdu <xuanghdu@outlook.com>
2021-02-20 01:59:08 -0500

    me

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export const Me = (props) => {
     return (
         <View style={styles.top}>
-        <View style={styles.row}>
-        <View style={styles.container}>
-            <Image
-                style={styles.avatar}
-                source={{
-                    uri: 'https://reactnative.dev/img/tiny_logo.png',
-                }}
+            <Text style={styles.header}>Personal Profile</Text>
+            <View style={styles.row}>
+                <View style={styles.container}>
+                    <Image
+                        style={styles.avatar}
+                        source={{
+                            uri: 'https://reactnative.dev/img/tiny_logo.png',
+                        }}
+                    />
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.field}>USERNAME: {props.username}</Text>
+                    <Text style={styles.field}>Name: {props.name}</Text>
+                    <Text style={styles.field}>Date of Birth: {props.dob}</Text>
+                    <Text style={styles.field}>Gender: {props.gender}</Text>
+                    <Text style={styles.field}>Country/Region: {props.nat}</Text>
+                    <Text style={styles.field}>Contact: {props.contact}</Text>
+                </View>
+            </View>
+            <Text style={styles.header}>Set A New Goal</Text>
+            <Text style={styles.text}>Describe your new goal here:</Text>
+            <Text style={styles.text}>Do you want to invite others?</Text>
+            <Button
+                onPress={SetGoal}
+                title="Set Your Goal"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
             />
-        </View>
-        <View style={styles.container}>
-            <Text style={styles.text}>USERNAME: {props.username}</Text>
-            <Text style={styles.text}>Name: {props.name}</Text>
-            <Text style={styles.text}>Date of Birth: {props.dob}</Text>
-            <Text style={styles.text}>Gender: {props.gender}</Text>
-            <Text style={styles.text}>Country/Region: {props.nat}</Text>
-            <Text style={styles.text}>Contact: {props.contact}</Text>
-        </View>
-        </View>
+            <Text style={styles.header}>Achievements</Text>
         </View>
     );
 }
+
+const SetGoal = () => {}
 
 const styles = StyleSheet.create({
     top: {
@@ -32,6 +45,10 @@ const styles = StyleSheet.create({
     },
     row: {
         flexDirection: "row",
+    },
+    header: {
+        color: 'chocolate',
+        fontWeight: 'bold',
     },
     container: {
         margin: '10px',
@@ -43,9 +60,14 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
     },
-    text: {
+    field: {
         fontFamily: 'Garamond',
-        // fontWeight: 'bold',
-        color: 'white',
+        color: 'beige',
+    },
+    text: {
+        margin: "10px",
+        padding: "10px",
+        fontFamily: 'Garamond',
+        color: 'beige',
     }
 });
