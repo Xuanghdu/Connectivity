1544303e5b2948c826b764a9eaba6a384042e56f
LittleDijkstraZ <54603864+LittleDijkstraZ@users.noreply.github.com>
2021-02-20 10:25:19 -0500

    cneter

--- src/components/Login/Register_screen.js
+++ src/components/Login/Register_screen.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
         margin: '10px',
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'bottom',
+        justifyContent: 'center',
     },
     avatar: {
         width: 100,
