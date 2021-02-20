55dfd9748b435b90ee1a2604703a252a4644cfaf
LittleDijkstraZ <54603864+LittleDijkstraZ@users.noreply.github.com>
2021-02-19 23:00:27 -0500

    hello world

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>UofTHacks VIII Starts Now!</Text>
+      <Text>UofTHacks VIII Starts Now! Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
